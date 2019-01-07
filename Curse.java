67e820f45068cc7cb2050dabd28e8ff81a4c542b
vanHooydonk <rootles@gmail.com>
2019-01-07 13:35:31 -0500

    Update Curse.java

--- Curse.java
+++ Curse.java
@@ -1,5 +1,18 @@
 public class Curse{
+
     String curseName;
+    String imageFilePath;
     bonusType affectsBonus;
     int affectAmount;
+
+    Curse (){
+
+    }
+
+    Curse (String cName, String imgPath, bonusType b, int amount){
+        curseName = cName;
+        imageFilePath = imgPath;
+        affectsBonus = b;
+        affectAmount = amount;
+    }
 }
